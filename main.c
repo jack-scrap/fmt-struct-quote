fb395acde835c7200d23c8116a74f208bdfa029c
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-10-02 16:38:48 -0600

    Output quote

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+
+const char sep = '-';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc != 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%s %c %s", argv[1], sep, argv[2]);
+
 	return 0;
 }
